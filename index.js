8f9b05767fe5dbb8660b90740296245d43aba556
Ubuntu <ubuntu@ip-172-31-42-51.ap-south-1.compute.internal>
2026-02-14 12:46:41 +0000

    modify index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("🚀 Node App running via Jenkins + Docker");
+  res.send(" hello guys this is my first project , now i will deploy multiple project for brushup my skills to get a better job for my career.");
 });
 
 app.get("/health", (req, res) => {
