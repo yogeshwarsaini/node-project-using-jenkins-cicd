2b3b2da5eb330b98c05986353224dba96500d557
Ubuntu <ubuntu@ip-172-31-42-51.ap-south-1.compute.internal>
2026-02-14 17:19:58 +0000

    updated index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send(" hello guys this is my first project , now i will deploy multiple project for brushup my skills to get a better job for my career.");
+  res.send(" hello guys this is my first project , now i will deploy multiple project for brushup my skills to get a better job for my career.<h1> mera name yogeshwar saini hai </h1>");
 });
 
 app.get("/health", (req, res) => {
